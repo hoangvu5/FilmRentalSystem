d726a647f20b3aef1a88131fd277d3ee45432951
Hoang Vu <minhhoangvu0505@gmail.com>
2024-06-10 08:01:11 +0700

    Add user login page

---
+++ src/main/java/com/mvms/movie_management_system/api/security/JWTRequestFilter.java
@@ -0,0 +1,53 @@
+package com.mvms.movie_management_system.api.security;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.mvms.movie_management_system.entity.User;
+import com.mvms.movie_management_system.repository.UserRepository;
+import com.mvms.movie_management_system.service.JWTService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Optional;
+
+@Component
+public class JWTRequestFilter extends OncePerRequestFilter {
+    private JWTService jwtService;
+    private UserRepository userRepository;
+
+    public JWTRequestFilter(JWTService jwtService, UserRepository userRepository) {
+        this.jwtService = jwtService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String tokenHeader = request.getHeader("Authorization");
+        if (tokenHeader != null && tokenHeader.startsWith("Bearer ")) {
+            String token = tokenHeader.substring(7);
+            try {
+                String username = jwtService.getUsername(token);
+                Optional<User> dbUser = userRepository.findByUsernameIgnoreCase(username);
+                if (dbUser.isPresent()) {
+                    User user = dbUser.get();
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } catch (JWTDecodeException e) {
+
+            }
+
+        }
+        filterChain.doFilter(request, response);
+    }
+}
